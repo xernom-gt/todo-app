f330f392a26ca12a5f4b2c64d9c19c571ced1228
xernom-gt <dasboard10@gmail.com>
2025-12-17 22:34:15 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\HistoryController;
 // Guest Routes
 Route::middleware('guest')->group(function () {
     Route::get('/', [AuthController::class, 'showLogin'])->name('login');
-    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/', [AuthController::class, 'login']);
     Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
     Route::post('/register', [AuthController::class, 'register']);
 });
